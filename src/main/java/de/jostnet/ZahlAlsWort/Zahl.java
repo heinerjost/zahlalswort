b58bc3d3393dfe6251e586ce23ef123db2faf19e
Heiner <heiner@jost-net.de>
2023-03-24 15:55:07 +0100

    Scope eingeschränkt

--- src/main/java/de/jostnet/ZahlAlsWort/Zahl.java
+++ src/main/java/de/jostnet/ZahlAlsWort/Zahl.java
@@ -1,27 +1,18 @@
 package de.jostnet.ZahlAlsWort;
+
 /**********************************************************************
  * Copyright (c) by Heiner Jostkleigrewe
- * This program is free software: you can redistribute it and/or modify it under the terms of the 
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without 
- *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
- *  the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.  If not, 
- * see <http://www.gnu.org/licenses/>.
  * 
  * heiner@jost-net.de
  **********************************************************************/
 
-public class Zahl
+class Zahl
 {
 	private int zahl;
 
 	private String zahlenstring;
 
-	public Zahl(int zahl)
+	Zahl(int zahl)
 	{
 		this.zahl = zahl;
 		this.zahlenstring = zahl + "";
@@ -56,7 +47,6 @@ public class Zahl
 	{
 		return zahlenstring.length() >= 7 && zahlenstring.length() <= 9;
 	}
-
 
 	public boolean isMillion()
 	{
--- src/test/java/de/jostnet/ZahlAlsWort/ZahlAlsWortTest.java
+++ src/test/java/de/jostnet/ZahlAlsWort/ZahlAlsWortTest.java
@@ -2,21 +2,13 @@ package de.jostnet.ZahlAlsWort;
 
 /**********************************************************************
  * Copyright (c) by Heiner Jostkleigrewe
- * This program is free software: you can redistribute it and/or modify it under the terms of the 
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY; without 
- *  even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
- *  the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.  If not, 
- * see <http://www.gnu.org/licenses/>.
  * 
  * heiner@jost-net.de
  **********************************************************************/
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
 
@@ -252,6 +244,24 @@ public class ZahlAlsWortTest
 	{
 		assertEquals("vierhundertvierundvierzigmillioneneinhunderttausend",
 				ZahlAlsWort.get(444100000));
+	}
+
+	@Test
+	void fuenfundfuenfzigBigDecimal()
+	{
+		assertEquals("fünfundfünfzig", ZahlAlsWort.get(new BigDecimal("55.66")));
+	}
+
+	@Test
+	void fuenfundfuenfzigDouble()
+	{
+		assertEquals("fünfundfünfzig", ZahlAlsWort.get(Double.valueOf(55.66)));
+	}
+
+	@Test
+	void fuenfundfuenfzigFloat()
+	{
+		assertEquals("fünfundfünfzig", ZahlAlsWort.get(Float.valueOf(55.66f)));
 	}
 
 }
